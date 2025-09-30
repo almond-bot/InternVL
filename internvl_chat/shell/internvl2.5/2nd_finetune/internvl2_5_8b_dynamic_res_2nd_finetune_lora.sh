abda0b91ab5ba8b1d5e9e9bcf9652511214d5db6
Almond Lambda <lambda@almondbot.com>
2025-09-30 02:53:39 +0000

    add ai training done

--- internvl_chat/shell/internvl2.5/2nd_finetune/internvl2_5_8b_dynamic_res_2nd_finetune_lora.sh
+++ internvl_chat/shell/internvl2.5/2nd_finetune/internvl2_5_8b_dynamic_res_2nd_finetune_lora.sh
@@ -73,3 +73,5 @@ torchrun \
   --deepspeed "zero_stage1_config.json" \
   --report_to "tensorboard" \
   2>&1 | tee -a "${OUTPUT_DIR}/training_log.txt"
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"AI training done!"}' https://hooks.slack.com/services/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
