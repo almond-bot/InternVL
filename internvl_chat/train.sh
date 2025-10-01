3f1289648367440d39f23264acb508100eba5770
Shawn Patel <shawnptl8@gmail.com>
2025-10-01 16:02:36 -0500

    remove human_zed_in_bowl from git

--- internvl_chat/train.sh
+++ internvl_chat/train.sh
@@ -15,6 +15,11 @@ fi
 hf download OpenGVLab/InternVL2_5-8B --local-dir pretrained/InternVL2_5-8B
 hf download almond-bot/human_zed_in_bowl --local-dir ~/human_zed_in_bowl --repo-type dataset
 
+# Count lines in annotations.jsonl and update the JSON config
+LINE_COUNT=$(wc -l < ~/human_zed_in_bowl/annotations.jsonl)
+sed -i.bak "s/\"length\": \"[0-9]*\"/\"length\": \"$LINE_COUNT\"/" shell/data/human_zed_in_bowl.json
+rm shell/data/human_zed_in_bowl.json.bak
+
 rm -fr work_dirs
 nohup bash -c "GPUS=1 PER_DEVICE_BATCH_SIZE=4 bash shell/internvl2.5/2nd_finetune/internvl2_5_8b_dynamic_res_2nd_finetune_lora.sh" > /dev/null 2>&1 &
 disown
