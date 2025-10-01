f025fd300645c08091f5e716fe3b5929852fd955
Shawn Patel <shawnptl8@gmail.com>
2025-09-30 23:04:51 -0500

    increase per device batch size

--- internvl_chat/train.sh
+++ internvl_chat/train.sh
@@ -11,5 +11,5 @@ mkdir pretrained
 hf download OpenGVLab/InternVL2_5-8B --local-dir pretrained/InternVL2_5-8B
 hf download almond-bot/human_zed_in_bowl --local-dir ~/human_zed_in_bowl --repo-type dataset
 
-nohup bash -c "GPUS=1 PER_DEVICE_BATCH_SIZE=6 bash shell/internvl2.5/2nd_finetune/internvl2_5_8b_dynamic_res_2nd_finetune_lora.sh" > /dev/null 2>&1 &
+nohup bash -c "GPUS=1 PER_DEVICE_BATCH_SIZE=8 bash shell/internvl2.5/2nd_finetune/internvl2_5_8b_dynamic_res_2nd_finetune_lora.sh" > /dev/null 2>&1 &
 disown
